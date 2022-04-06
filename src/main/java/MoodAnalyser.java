7f7edab95d5e0ba1c421a5ef142bdf82b728940c
AdeshMask <adeshmaske255@gmail.com>
2022-04-06 08:57:21 +0530

    Added:Given message and returning sad

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,29 +1,16 @@
-import java.util.*;
-
 
 public class MoodAnalyser {
-    List<String> list = new ArrayList<>();
-    String sad1 = "Sad";
-    String sad2 = "sad";
+    public String message;
 
-    public static void main(String[] args) {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        System.out.println("Welcome to MoodAnalyser:");
-        String str = "I am in any mood";
-        System.out.println(str);
-        System.out.println(moodAnalyser.analyseMood(str));
-    }
-
-    private String analyseMood(String str) {
-        String find = "sad";
-        String mood = null;
-        int i = str.indexOf(find);
-        if(i>0) {
-            mood = find;
+    public MoodAnalyser(String message){
+        this.message = message;
+        }
+    public  String analyseMood() {
+        if(this.message.toLowerCase().contains("sad")) {
+            return "sad";
         }
         else {
-            mood = "Happy";
+            return  "Happy";
         }
-        return mood;
     }
 }
