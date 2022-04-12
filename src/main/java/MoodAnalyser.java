458c57cc7254456eba85c8efd4094ffeb2695650
AdeshMask <adeshmaske255@gmail.com>
2022-04-12 15:01:29 +0530

    Added:MoodAnalysisException

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,22 +1,38 @@
 
-public class MoodAnalyser {
-    public String message;
+class MoodAnalysisException extends Exception{
+    String message;
+    public MoodAnalysisException(String message){
+        this.message = message;
+    }
+}
 
-    public MoodAnalyser(){
+public class MoodAnalyser {
+    public static String message;
+
+    public MoodAnalyser(String s){
     };
-    public MoodAnalyser(String message){
+
+    public void MoodAnalysisException(String message){
         this.message = message;
         }
-    public  String analyseMood() {
+    public  String analyseMood() throws MoodAnalysisException {
         try {
-            if(this.message.toLowerCase().contains("sad")) {
-                return "sad";
-            }
-            else {
-                return  "Happy";
-            }
-        } catch (NullPointerException e) {
+            if (this.message.toLowerCase().contains("sad")) {
+                throw new MoodAnalysisException("Invalid Mood");
+            } else if (this.message.toLowerCase().contains("Happy")) {
+                return "Happy";
+            } else if (message.isEmpty() || message == null){
                 return "Happy(null)";
+            }
+            else
+                return "No match";
+        }
+        catch (NullPointerException e) {
+            throw new MoodAnalysisException("Invalid Mood");
+        }
+        catch (MoodAnalysisException e) {
+            e.printStackTrace();
+            return "Invalid Mood";
         }
     }
 }
